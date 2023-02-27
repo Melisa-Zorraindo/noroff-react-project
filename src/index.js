4e7d4750ca7406dc64ca5f57e3ce87b2343c352d
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-02-27 18:09:18 +0100

    Adds global styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./styles/GlobalStyles";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>
