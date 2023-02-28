c8ff8d4fbade2546a77eb953cca1d77622ad70ac
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-02-28 14:42:07 +0100

    Renders SearchBar and Cart in Header

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,4 +1,6 @@
 import Nav from "../Nav";
+import SearchBar from "../SearchBar";
+import Cart from "../Cart";
 import Logo from "../../../assets/logo.png";
 import { StyledHeader } from "./styles";
 
@@ -6,6 +8,8 @@ export default function Header() {
   return (
     <StyledHeader>
       <img src={Logo} alt="techtopia logo" />
+      <SearchBar />
+      <Cart />
       <Nav />
     </StyledHeader>
   );
