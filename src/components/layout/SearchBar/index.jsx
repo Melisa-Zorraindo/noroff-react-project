29fef191cf8a92597ffdb0c780acca3ba8782aca
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-02-28 14:38:50 +0100

    Adds search bar component

---
+++ src/components/layout/SearchBar/index.jsx
@@ -0,0 +1,9 @@
+export default function SearchBar() {
+  return (
+    <>
+      <label htmlFor="search-bar">
+        <input type="text" placeholder="Search" name="search" />
+      </label>
+    </>
+  );
+}
