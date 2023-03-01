12c4174ecf7eb2cfee928eceff509f0bfa74f777
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-01 13:34:17 +0100

    Fixes error on console

--- src/components/layout/SearchBar/index.jsx
+++ src/components/layout/SearchBar/index.jsx
@@ -2,7 +2,7 @@ export default function SearchBar() {
   return (
     <>
       <label htmlFor="search-bar">
-        <span class="material-symbols-rounded">search</span>
+        <span className="material-symbols-rounded">search</span>
         <input type="text" placeholder="Search" name="search" />
       </label>
     </>
