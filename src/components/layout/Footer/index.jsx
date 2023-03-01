ac633e4ed1850099ab6c61a0660d64ad7a57573b
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-01 16:49:57 +0100

    Adds Footer

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -1,3 +1,21 @@
+import { StyledFooter } from "./styles";
+import { Link } from "react-router-dom";
+
 export default function Footer() {
-  return <h1>This is my footer</h1>;
+  return (
+    <StyledFooter>
+      <h4>&copy; Melisa Zorraindo</h4>
+      <ul>
+        <li>
+          <Link to="https://github.com/Melisa-Zorraindo">GitHub</Link>
+        </li>
+        <li>·</li>
+        <li>
+          <Link to="https://www.linkedin.com/in/melisa-zorraindo-81719618b/">
+            LinkedIn
+          </Link>
+        </li>
+      </ul>
+    </StyledFooter>
+  );
 }
---
+++ src/components/layout/Footer/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  border-top: 1px solid #e0e2e2;
+  text-align: center;
+  padding-bottom: 1rem;
+
+  ul {
+    display: flex;
+    list-style: none;
+    justify-content: center;
+    gap: 1rem;
+  }
+
+  a {
+    color: #1f1f1f;
+  }
+`;
