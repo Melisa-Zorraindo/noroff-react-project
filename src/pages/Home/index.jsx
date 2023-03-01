056e5db3979befed72bb1630af08c4db1c792910
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-01 17:58:50 +0100

    Displays products on homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { StyledHomePage } from "./styles";
+import ProductCard from "../../components/ProductCard";
+
+const url = "https://api.noroff.dev/api/v1/online-shop";
+
 export default function Home() {
-  return <h1>Welcome to my homepage</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome to my homepage</h1>
+      <StyledHomePage>
+        {products.map((product) => {
+          return (
+            <ProductCard
+              key={product.id}
+              imageUrl={product.imageUrl}
+              title={product.title}
+              price={product.price}
+            />
+          );
+        })}
+      </StyledHomePage>
+    </>
+  );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledHomePage = styled.div`
+  margin: 4rem 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+`;
