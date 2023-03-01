e12c543e13abb161da330b03fe21faa4c3ba5212
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-01 16:57:45 +0100

    Adds link from logo to homepage

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,13 +1,13 @@
 import Nav from "../Nav";
 import SearchBar from "../SearchBar";
 import Cart from "../Cart";
-import Logo from "../../../assets/logo.png";
+import Logo from "../Logo";
 import { StyledHeader } from "./styles";
 
 export default function Header() {
   return (
     <StyledHeader>
-      <img src={Logo} alt="techtopia logo" />
+      <Logo />
       <SearchBar />
       <Cart />
       <Nav />
