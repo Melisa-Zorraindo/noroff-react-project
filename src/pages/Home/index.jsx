66a81d6dded20e769e92994c1541df38864e01ce
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-01 22:08:39 +0100

    Fixes h1 in homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -23,8 +23,8 @@ export default function Home() {
 
   return (
     <>
-      <h1>Welcome to my homepage</h1>
       <StyledHomePage>
+        <h1>All products</h1>
         {products.map((product) => {
           return (
             <ProductCard
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,8 +2,14 @@ import styled from "styled-components";
 
 export const StyledHomePage = styled.div`
   margin: 4rem 0;
+  padding: 0 1rem;
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
   gap: 2rem;
+
+  h1 {
+    flex: 100%;
+    padding-left: 1rem;
+  }
 `;
