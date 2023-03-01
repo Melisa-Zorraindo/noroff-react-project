3a65dfe002b90ae44f4cc8f3a17f20d1756ea210
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-01 13:26:12 +0100

    Adds icon to search bar

--- src/components/layout/SearchBar/index.jsx
+++ src/components/layout/SearchBar/index.jsx
@@ -2,6 +2,7 @@ export default function SearchBar() {
   return (
     <>
       <label htmlFor="search-bar">
+        <span class="material-symbols-rounded">search</span>
         <input type="text" placeholder="Search" name="search" />
       </label>
     </>
