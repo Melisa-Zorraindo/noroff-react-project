8ccb4427396bb876e28816306c19f2f7eb4419a5
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-15 19:52:00 +0100

    Updates page title

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import { StyledContactPage } from "./styles";
 import ContactForm from "../../components/ContactForm";
 
 export default function Contact() {
+  useEffect(() => {
+    document.title = "Techtopia | Get in touch";
+  }, []);
+
   return (
     <StyledContactPage>
       <h1>Get in touch</h1>
