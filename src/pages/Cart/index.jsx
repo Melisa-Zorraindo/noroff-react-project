05b8ea981f309cf52d88ad7b7565795a3d812b86
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-15 09:29:40 +0100

    Creates OrderSummary component

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -4,12 +4,13 @@ import { useProductsStore } from "../../utils/stateManagement";
 import { shallow } from "zustand/shallow";
 import { StyledCartPage } from "./styles";
 import CartItem from "../../components/CartItem";
-import PrimaryButton from "../../components/PrimaryButton";
+import OrderSummary from "../../components/OrderSummary";
 
 export default function Cart() {
   useEffect(() => {
     document.title = "Techtopia | Shopping cart";
   }, []);
+
   const { availableProducts, clearCart } = useProductsStore(
     (state) => ({
       availableProducts: state.availableProducts,
@@ -65,26 +66,12 @@ export default function Cart() {
               />
             ))}
           </div>
-          <div className="checkout-container">
-            <h3>Order summary</h3>
-            <div className="price-summary">
-              <p>Subtotal</p>
-              <p>{calcSubtotal()} NOK</p>
-            </div>
-            <div className="price-summary">
-              <p>Shipping</p>
-              <p>Free</p>
-            </div>
-            <div className="price-summary">
-              <p>VAT 25%</p>
-              <p>{calcVAT()} NOK</p>
-            </div>
-            <div className="price-summary">
-              <p className="total">Total</p>
-              <p className="total">{calcTotal()} NOK</p>
-            </div>
-            <PrimaryButton onClick={() => clearCart()} text={"Clear cart"} />
-          </div>
+          <OrderSummary
+            calcSubtotal={calcSubtotal}
+            calcVAT={calcVAT}
+            calcTotal={calcTotal}
+            clearCart={clearCart}
+          />
         </>
       ) : (
         <div>
--- src/pages/Cart/styles.jsx
+++ src/pages/Cart/styles.jsx
@@ -16,38 +16,8 @@ export const StyledCartPage = styled.main`
     flex: 0 0 65%;
   }
 
-  .checkout-container {
-    border: ${({ theme }) =>
-      `${theme.border.thickness} ${theme.border.type} ${theme.color.support}`};
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    margin: 2rem 0;
-    height: fit-content;
-    flex: 0 0 30%;
-
-    h3 {
-      margin: 0 0 1rem;
-    }
-  }
-
-  .price-summary {
-    display: flex;
-    justify-content: space-between;
-
-    p {
-      margin: 0.5rem 0;
-    }
-
-    .total {
-      font-weight: 700;
-      margin: 1rem 0;
-    }
-  }
-
   @media (max-width: ${({ theme }) => theme.break.medium}) {
-    .purchase,
-    .checkout-container {
+    .purchase {
       flex: 0 0 100%;
     }
   }
